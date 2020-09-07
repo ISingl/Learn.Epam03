b138cf35268a3ef6cba886fb80be865651f217d3
Nogin Sergey <ver-s_online@mail.ru>
2020-09-08 00:39:03 +0400

    init

---
+++ Task1_GCD/GCD.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_GCD
+{
+    public class GCD
+    {
+        /// <summary>
+        /// The Euclidean algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers (numbers),
+        /// the largest number that divides them both without a remainder.
+        /// </summary>
+        /// <param name="a">The first integer</param>
+        /// <param name="b">The second integer</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int EuclidAlgorithm(int a, int b)
+        {
+            if (a == 0 && b == 0) throw new DivideByZeroException();
+            if (a == b || b == 0) return a;
+            if (a == 0) return b;
+
+            if (a < 0) a *= -1;
+            if (b < 0) b *= -1;
+
+            while (a != b)
+            {
+                if (a > b)
+                    a -= b;
+                else
+                    b -= a;
+            }
+
+            return a;
+        }
+
+        /// <summary>
+        /// The Euclidean algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers (numbers),
+        /// the largest number that divides them both without a remainder.
+        /// </summary>
+        /// <param name="a">The first integer</param>
+        /// <param name="b">The second integer</param>
+        /// <param name="c">The third integer</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int EuclidAlgorithm(int a, int b, int c)
+        {
+            return EuclidAlgorithm(new int[] { a, b, c });
+        }
+
+        /// <summary>
+        /// The Euclidean algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers (numbers),
+        /// the largest number that divides them both without a remainder.
+        /// </summary>
+        /// <param name="a">The first integer</param>
+        /// <param name="b">The second integer</param>
+        /// <param name="c">The third integer</param>
+        /// <param name="d">The fourth integer</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int EuclidAlgorithm(int a, int b, int c, int d)
+        {
+            return EuclidAlgorithm(new int[] { a, b, c, d });
+        }
+
+        /// <summary>
+        /// The Euclidean algorithm is an efficient method for computing the greatest common divisor (GCD) of two integers (numbers),
+        /// the largest number that divides them both without a remainder.
+        /// </summary>
+        /// <param name="a">The first integer</param>
+        /// <param name="b">The second integer</param>
+        /// <param name="c">The third integer</param>
+        /// <param name="d">The fourth integer</param>
+        /// <param name="e">The fifth integer</param>
+        /// <returns>Greatest common divisor</returns>
+        public static int EuclidAlgorithm(int a, int b, int c, int d, int e)
+        {
+            return EuclidAlgorithm(new int[] { a, b, c, d, e });
+        }
+
+        private static int EuclidAlgorithm(params int[] numbers)
+        {
+            if (numbers.Length == 0) throw new ArgumentNullException();
+            if (numbers.Length == 1) return numbers[0];
+
+            int tmp = numbers[0];
+            foreach (int value in numbers)
+                tmp = EuclidAlgorithm(tmp, value);
+
+            return tmp;
+        }
+    }
+}
